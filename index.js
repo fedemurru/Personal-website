daceaf758b8268082fe0fd4bd4d0e8cb364739b5
Fedemurru <djfedemurru@icloud.com>
2022-11-28 14:45:24 +0000

    commit 2

---
+++ index.js
@@ -0,0 +1,16 @@
+var myFunction = (a,b) => {
+
+    if(a > b) {
+        console.log('A is bigger');
+    }
+else if (a<b){ 
+ console.log('B is bigger');
+}
+else {
+    console.log('they are equal');
+}
+}
+
+
+myFunction(10,9);
+
